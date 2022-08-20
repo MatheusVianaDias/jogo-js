8046a6ae80f3ed4dab1c76e284dae13b7eb8aff8
VianaDias <109551825+MatheusVianaDias@users.noreply.github.com>
2022-08-20 01:19:06 -0300

    jogo-one-piece

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let mario = document.querySelector('.mario');
+let pipe = document.querySelector('.pipe');
+
+console.log(mario);
+let jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+    
+    },500);
+}
+
+let loop = setInterval(()=>{
+
+let pipePosition = pipe.offsetLeft;
+let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+console.log(marioPosition);
+
+if (pipePosition <= 90 && pipePosition > 0 && marioPosition < 50 ){
+   
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom= `${marioPosition}px`;
+
+    mario.src = "img/luffyderrota.png"
+    mario.style.width = '60px'
+    mario.style.height = '60px'
+    mario.style.marginLeft = '50px'
+
+    clearInterval(loop);
+
+}
+
+}, 10)
+
+document.addEventListener('keydown', jump);
